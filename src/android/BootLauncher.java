916b8531bcfee660820d3b9b2fe30a661600cbd4
vaghtonline <vaghtonline.com@gmail.com>
2016-05-21 10:07:37 +0430

    Update BootLauncher.java

--- src/android/BootLauncher.java
+++ src/android/BootLauncher.java
@@ -10,7 +10,7 @@ public class BootLauncher extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent serviceIntent = new Intent(context, com.vaghtonline.androapp.CordovaApp.class);
+        Intent serviceIntent = new Intent(context, com.vaghtonline.androapp.MainActivity.class);
         serviceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(serviceIntent);
     }
